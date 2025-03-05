9e62328393813a7f5dcf37fccb4a4ee2cce42718
Hariprasath1911 <hari07011996@gmail.com>
2025-03-05 16:33:59 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,54 @@
+import streamlit as st
+import pandas as pd 
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import OrdinalEncoder
+import tensorflow as tf
+import pickle
+import base64
+
+def set_background_image_local(image_path):
+    with open(image_path, "rb") as file:
+        data = file.read()
+    base64_image = base64.b64encode(data).decode("utf-8")
+    st.markdown(
+        f"""
+        <style>
+        .stApp {{
+            background-image: url("data:image/png;base64,{base64_image}");
+            background-size: contain;
+            background-position: fit;
+            background-repeat: repeat;
+            background-attachment: fixed;
+        }}     
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+set_background_image_local(r"12.png")
+
+def load_model(model_path):
+    with open(model_path, 'rb') as file:
+        return pickle.load(file)
+encoder=load_model("Encoder_MP.pkl")
+scaler=load_model("scaler.pkl")
+model=load_model("model_final.h5")
+uploaded_file = st.file_uploader("Upload an Excel file", type=["xlsx", "xls"])
+
+if uploaded_file is not None:
+    try:
+        df = pd.read_excel(uploaded_file)
+
+        if 'Machining_Process' in df.columns:
+            df['Machining_Process'] = encoder.transform(df["Machining_Process"])
+        else:
+            st.error("Column 'Machining_Process' not found in the uploaded file.")
+
+        df_scaled = pd.DataFrame(scaler.transform(df), columns=df.columns)
+        
+    except Exception as e:
+        st.error(f"Error: {e}")
+if st.button('Predict'):
+    prediction = model.predict(df_scaled)
+    st.subheader("Predicted Car Price")
+    st.markdown(f"### :green[{prediction}]")
